ab72ea94405f9ffe1fcdeb8f62bd1fc7e7258930
dijac <darshanbhusal41@gmail.com>
2021-10-04 13:55:41 +0545

    webbrowser google/youtube

--- softwarica.py
+++ softwarica.py
@@ -2,6 +2,7 @@ import pyttsx3
 import datetime
 import speech_recognition as sr
 import wikipedia
+import webbrowser
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -72,3 +73,11 @@ if __name__ == '__main__':
             speak("according to wikipedia ")
             print(results)
             speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open('youtube.com')
+
+        elif 'open google' in query:
+            webbrowser.open('google.com')
+
+
